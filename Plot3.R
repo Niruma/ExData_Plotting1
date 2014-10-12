4f8f72b627ea6b0246acdc41edd30aa3bee8661e
Niran Iyer <nnjamb@monsanto.com>
2014-10-12 11:20:21 -0500

    added files

--- Plot3.R
+++ Plot3.R
@@ -7,8 +7,8 @@ newdata$Sub_metering_3[newdata$Sub_metering_3=="?"] <- NA
 newdata1 <- na.omit(newdata)
 head(newdata1)
 plot(newdata1$Datetime, newdata1$Sub_metering_1, col="black", type="l",xlab = "", ylab = "Energy sub metering")
-points(newdata1$Datetime, newdata1$Sub_metering_2, type="l", pch=22, lty=2, col="red",xlab = "", ylab = "Energy sub metering")
-points(newdata1$Datetime, newdata1$Sub_metering_3, type="l", pch=22, lty=2, col="blue", xlab = "", ylab = "Energy sub metering")
+points(newdata1$Datetime, newdata1$Sub_metering_2, type="l",  col="red",xlab = "", ylab = "Energy sub metering")
+points(newdata1$Datetime, newdata1$Sub_metering_3, type="l",  col="blue", xlab = "", ylab = "Energy sub metering")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", 
                                                                     "Sub_metering_2", "Sub_metering_3"))
 png("plot3.png", width=480, height=480)
